022d62de616b35f1e44de7be8ed08a14a604bcfb
Chris Green <hello@direct-root.com>
2025-04-01 14:58:20 +0100

    start of explicit definitions

--- dagster_project/assets.py
+++ dagster_project/assets.py
@@ -54,48 +54,55 @@ CLIENTS_DB = [
         })
 ]
 
-def assets_factory(clients_db: List[Client]):
-    assets = []
-    for client in clients_db:
-        for config_class in DLT_SOURCES:
-            environ[f'{client.id}_{config_class.name.upper()}__BUCKET_URL'] = f'./data/{client.id}'
-
-
-            @dlt.source(name=f'{client.id}_{config_class.name}')
-            def source_func():
-                config = config_class(client.org_url, client.api_token)
-                resources = rest_api_resources(config.rest) # returns a variable number of resources depending on REST config
-                for resource in resources: # add all the data maps for the given resource
-                    for map in config.data_maps:
-                        resource.add_map(
-                            lambda data: map(data)
-                        )
-                
-                yield from resources
-
-
-            @dlt_assets(
-                dlt_source=source_func(),
-                dlt_pipeline=dlt.pipeline(
-                    pipeline_name=f'{client.id}_{config_class.name}',
-                    destination='filesystem',
-                    dataset_name=config_class.name
-                ),
-                name=f'{client.id}_{config_class.name}',
-                group_name=f'{client.dagster_safe_prefix}_okta'
+def add_data_maps(resources, config):
+    for resource in resources: # add all the data maps for the given resource
+        for map in config.data_maps:
+            resource.add_map(
+                lambda data: map(data)
             )
-            def okta_user_assets(context: AssetExecutionContext, dlt: DagsterDltResource):
-                yield from dlt.run(context=context)
-
-            assets.append(okta_user_assets)
-
-    return assets
+    return resources
 
 
-defs = dg.Definitions(
-    assets=assets_factory(CLIENTS_DB),
-    resources={
-        "dlt": DagsterDltResource(),
-    },
-)
+def definitions_for_a_single_client(client: Client, dlt_resource: DagsterDltResource):
+    
+    # Okta Users
+    @dlt.source(name=f'{client.id}_{OktaUsers.name}')
+    def okta_users_source():
+        config = OktaUsers(client.org_url, client.api_token)
+        environ[f'{client.id}_{config.name.upper()}__BUCKET_URL'] = f'./data/{client.id}'
+        resources = rest_api_resources(config.rest) # returns a variable number of resources depending on REST config
+        resources = add_data_maps(resources, config)
+        
+        yield from resources
 
+    @dlt_assets(
+        dlt_source=okta_users_source(),
+        dlt_pipeline=dlt.pipeline(
+            pipeline_name=f'{client.id}_{OktaUsers.name}',
+            destination='filesystem',
+            dataset_name=OktaUsers.name
+        ),
+        name=f'{client.id}_{OktaUsers.name}',
+        group_name=f'{client.dagster_safe_prefix}_okta'
+    )
+    def okta_user_assets(context: AssetExecutionContext, dlt: DagsterDltResource):
+        yield from dlt.run(context=context)
+
+
+
+
+    return dg.Definitions(
+            assets=[
+                okta_user_assets,
+            ],
+            resources={
+                'dlt': dlt_resource
+            }
+        )
+
+dlt_resource = DagsterDltResource()
+definitions_for_all_clients = []
+for client in CLIENTS_DB:
+    definitions_for_all_clients.append(definitions_for_a_single_client(client, dlt_resource))
+
+defs = dg.Definitions.merge(*definitions_for_all_clients)
